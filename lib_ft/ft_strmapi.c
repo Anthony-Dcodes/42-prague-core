74f510deb7678d79b2d70b202cefd17fd4159d9d
Anthony-Dv <dvoracekantonin@icloud.com>
2025-06-03 22:36:27 +0200

    ft_strmapi done

---
+++ lib_ft/ft_strmapi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/03 19:23:03 by advorace          #+#    #+#             */
+/*   Updated: 2025/06/03 19:31:08 by advorace         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	len;
+	char	*str;
+	size_t	i;
+
+	if (!s || !f)
+		return (NULL);
+	len = ft_strlen(s);
+	str = malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = f((unsigned int)i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
