67650ff00f37ebcb036628cbcb07a68339d4a8de
Anthony-Dv <dvoracekantonin@icloud.com>
2025-06-06 19:14:33 +0200

    ft_lstlast.c done and tested

--- lib_ft/libft.h
+++ lib_ft/libft.h
@@ -6,7 +6,7 @@
 /*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/24 10:59:30 by advorace          #+#    #+#             */
-/*   Updated: 2025/06/06 18:46:08 by advorace         ###   ########.fr       */
+/*   Updated: 2025/06/06 19:12:56 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,5 +59,6 @@ typedef struct s_list
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif
