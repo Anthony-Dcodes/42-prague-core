2f6c6189730e297de8c57960ac3841abded40ac3
Antonin Dvoracek <advorace@c3r1s2.42prague.com>
2025-06-29 18:25:12 +0200

    ft_lstmap working not norminette compliant

--- lib_ft/ft_lstmap_bonus.c
+++ lib_ft/ft_lstmap_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
+/*   By: advorace <advorace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 18:44:43 by advorace          #+#    #+#             */
-/*   Updated: 2025/06/08 12:07:18 by advorace         ###   ########.fr       */
+/*   Updated: 2025/06/29 18:21:15 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,23 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
 	t_list	*new_obj;
+	void	*new_content;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	new_lst = NULL;
 	while (lst)
 	{
-		new_obj = ft_lstnew((*f)(lst->content));
+		new_content = f(lst->content);
+		if (!new_content)
+		{
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		new_obj = ft_lstnew(new_content);
 		if (!new_obj)
 		{
+			del(new_content);
 			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
