9b4cf160729371b3727d55b9a805bad710c98723
Antonin Dvoracek <advorace@c3r1s2.42prague.com>
2025-06-30 18:18:00 +0200

    ft_lstmap fix

--- lib_ft/ft_lstmap_bonus.c
+++ lib_ft/ft_lstmap_bonus.c
@@ -6,11 +6,29 @@
 /*   By: advorace <advorace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 18:44:43 by advorace          #+#    #+#             */
-/*   Updated: 2025/06/29 18:21:15 by advorace         ###   ########.fr       */
+/*   Updated: 2025/06/30 18:17:24 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+// Helper function to create a new node (cnn) with transformed content
+static t_list	*cnn(void *content, void *(*f)(void *), void (*del)(void *))
+{
+	void	*new_content;
+	t_list	*new_node;
+
+	new_content = f(content);
+	if (!new_content)
+		return (NULL);
+	new_node = ft_lstnew(new_content);
+	if (!new_node)
+	{
+		del(new_content);
+		return (NULL);
+	}
+	return (new_node);
+}
 
 // Iterate through lst
 // Apply function f to content in each node
@@ -19,28 +37,20 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
-	t_list	*new_obj;
-	void	*new_content;
+	t_list	*new_node;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	new_lst = NULL;
 	while (lst)
 	{
-		new_content = f(lst->content);
-		if (!new_content)
+		new_node = cnn(lst->content, f, del);
+		if (!new_node)
 		{
 			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
-		new_obj = ft_lstnew(new_content);
-		if (!new_obj)
-		{
-			del(new_content);
-			ft_lstclear(&new_lst, del);
-			return (NULL);
-		}
-		ft_lstadd_back(&new_lst, new_obj);
+		ft_lstadd_back(&new_lst, new_node);
 		lst = lst->next;
 	}
 	return (new_lst);
