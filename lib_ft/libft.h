d949bfb1e906399e8a98679f9bad1d9fa66fc096
Antonin Dvoracek <advorace@c3r1s2.42prague.com>
2025-06-30 19:19:45 +0200

    libft.h fix

--- lib_ft/libft.h
+++ lib_ft/libft.h
@@ -3,18 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
+/*   By: advorace <advorace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/24 10:59:30 by advorace          #+#    #+#             */
-/*   Updated: 2025/06/07 19:30:07 by advorace         ###   ########.fr       */
+/*   Updated: 2025/06/30 19:18:20 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-#include <stddef.h>
-#include <stdlib.h>
+# include <stddef.h>
+# include <stdlib.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
