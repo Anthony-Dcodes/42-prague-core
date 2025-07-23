fadaf499d411a0ab10cd750bccd7c86ccca27759
Anthony-Dv <dvoracekantonin@icloud.com>
2025-07-23 19:47:22 +0200

    small malloc fix

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 17:40:27 by advorace          #+#    #+#             */
-/*   Updated: 2025/07/23 18:57:19 by advorace         ###   ########.fr       */
+/*   Updated: 2025/07/23 19:45:47 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,17 @@
 char	*get_next_line(int fd)
 {
 	char		buf[BUFFER_SIZE + 1];
-	static char	*stash = NULL;
+	static char	*stash;
 	size_t		bytes_read;
 	char		*line;
 
+	stash = NULL;
 	bytes_read = (read(fd, buf, BUFFER_SIZE));
 	while (bytes_read > 0)
 	{
 		buf[bytes_read] = '\0';
+		if (!stash)
+			stash = malloc(1);
 		stash = join_and_free(stash, buf);
 		if (ft_strchr(stash, '\n'))
 		{
