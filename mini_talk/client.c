b680f90f9a5b4cbb00a30c8197153b24413cde89
Antonin Dvoracek <advorace@c3r1s1.42prague.com>
2025-12-06 16:52:48 +0100

    fs

--- mini_talk/client.c
+++ mini_talk/client.c
@@ -3,17 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
+/*   By: advorace <advorace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 19:55:07 by advorace          #+#    #+#             */
-/*   Updated: 2025/12/05 20:21:27 by advorace         ###   ########.fr       */
+/*   Updated: 2025/12/06 14:06:17 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "client.h"
 
-int	main()
+int	main(int argc, char *argv[])
 {
-	ft_printf("Hey, this is client");
+	char	*pid;
+	char	*string;
+	
+	pid = argv[1];
+	string = argv[2];
+	ft_printf("Number of arguments: %i\n", argc);
+	ft_printf("Arg 0: %s\n", argv[0]);
+	ft_printf("Pid: %s\n", pid);
+	ft_printf("String: %s\n", string);
 	return (0);
 }
