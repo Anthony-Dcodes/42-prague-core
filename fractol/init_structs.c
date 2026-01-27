18d4fbd61a81a2ff4b2a840c4edd36c028dffa02
Antonin Dvoracek <advorace@c3r1s1.42prague.com>
2026-01-27 20:32:28 +0100

    init t_vars based on fractal_type

--- fractol/init_structs.c
+++ fractol/init_structs.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init_strucs.c                                      :+:      :+:    :+:   */
+/*   init_structs.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: advorace <advorace@student.42prague.com    +#+  +:+       +#+        */
+/*   By: advorace <advorace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 22:49:57 by advorace          #+#    #+#             */
-/*   Updated: 2026/01/26 22:52:57 by advorace         ###   ########.fr       */
+/*   Updated: 2026/01/27 20:31:55 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-void	init_t_vars(t_vars *vars)
+void	init_t_vars(t_vars *vars, char fractal_type)
 {
 	vars->win_width = 800;
 	vars->win_height = 600;
@@ -21,8 +21,18 @@ void	init_t_vars(t_vars *vars)
 	vars->circle_x = 500;
 	vars->circle_y = 500;
 	vars->circle_size = 100;
-	vars->x_min = MANDEL_X_MIN;
-	vars->x_max = MANDEL_X_MAX;
-	vars->y_min = MANDEL_Y_MIN;
-	vars->y_max = MANDEL_Y_MAX;
+	if (fractal_type == 'm')
+	{
+		vars->x_min = MANDEL_X_MIN;
+		vars->x_max = MANDEL_X_MAX;
+		vars->y_min = MANDEL_Y_MIN;
+		vars->y_max = MANDEL_Y_MAX;
+	}
+	else if (fractal_type == 'j')
+	{
+		vars->x_min = JULIA_X_MIN;
+		vars->x_max = JULIA_X_MAX;
+		vars->y_min = JULIA_Y_MIN;
+		vars->y_max = JULIA_Y_MAX;
+	}
 }
