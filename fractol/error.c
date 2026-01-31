749027b8b37694359bbd0cc089236fac9e98e169
Antonin Dvoracek <advorace@c3r1s1.42prague.com>
2026-01-31 13:01:38 +0100

    add burning ship to error

--- fractol/error.c
+++ fractol/error.c
@@ -6,7 +6,7 @@
 /*   By: advorace <advorace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 21:10:07 by advorace          #+#    #+#             */
-/*   Updated: 2026/01/27 21:50:11 by advorace         ###   ########.fr       */
+/*   Updated: 2026/01/31 13:01:21 by advorace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@ void	print_usage(void)
 	ft_putstr_fd("Usage:\n", 2);
 	ft_putstr_fd("  ./fractol m\n", 2);
 	ft_putstr_fd("  ./fractol j <real> <imag>\n", 2);
+	ft_putstr_fd("  ./fractol b", 2);
 	ft_putstr_fd("Hint: Julia values in range [-1.5, 1.5] look best.\n", 2);
 }
